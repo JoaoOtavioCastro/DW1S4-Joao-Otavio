5ecae4fb0d2abb2abdd57ee706ddd36a71a433bb
JoaoOtavioCastro <jotaviocastro359@gmail.com>
2023-09-21 11:33:52 -0300

    Adição lista de Paginas

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
-
-    // Array com os nomes das aulas
-    var aulas = ["Aula03", "Aula04", "Aula05", "Aula06", "Aula07", "Aula08", "Sabado"]; // Adicione quantas aulas você tiver
+    var aulas = ["Aula03", "Aula04", "Aula05", "Aula06", "Aula07", "Aula08", "Sabado"];     
     const titulo = document.getElementById("enderec");
     const endereco = document.getElementById("endereco");
-    // Inicializa o índice da aula atual
+    const lis = document.getElementById("lis")
     var currentIndex = 0;
 
-    // Função para navegar para a próxima aula
     function nextAula() {
         currentIndex++;
         if (currentIndex >= aulas.length) {
-            currentIndex = 0; // Volta para a primeira aula se atingir o final
+            currentIndex = 0;
         }
         updateIframe();
     }
 
-    // Função para navegar para a aula anterior
     function previousAula() {
         currentIndex--;
         if (currentIndex < 0) {
-            currentIndex = aulas.length - 1; // Volta para a última aula se estiver na primeira
+            currentIndex = aulas.length - 1;
         }
         updateIframe();
     }
 
-    // Atualiza o src do iframe com o caminho da aula atual
     function updateIframe() {
         var iframe = document.getElementById("iframe");
         var aulaName = aulas[currentIndex];
         iframe.src = "./" + aulaName + "/index.html";
         titulo.textContent = aulas[currentIndex];
+        endereco.setAttribute("href", "./"+aulaName+"/index.html");
     }
 
-    // Adiciona eventos de clique aos botões
     document.getElementById("leftButton").addEventListener("click", previousAula);
     document.getElementById("rightButton").addEventListener("click", nextAula);
 
+    let acumulador ="";
+aulas.forEach(element => {
+    acumulador += '<li><a href="./'+element+'/index.html" target="_blank">'+element+'</a></li>'
+});
+lis.innerHTML = acumulador;
 
-
-//    aulas.forEach(element => {
-  //  document.write("")
-//});
-
-
-    // Inicializa o iframe com a primeira aula
     updateIframe();
